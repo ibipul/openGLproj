44d335fb461581636fdcd2089ac6ef7cef70c1c1
ibipul <islam.bipul@gmail.com>
2014-02-12 10:26:10 +0530

    OpenGL first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Author:
+
+ - Bipul Islam
+
+----------------
+## Open GL 3D Cubes
+
+This is a very short open GL exposition that has the following
+ * a 3D stage with perspective,
+ * A camera to view the stage, 
+ * Three 3D cubes rotating in synch but arbitrarily.
+
+The programming is in C++ compiled in G++ compiler by GNU and uses OpenGL library calls to setup and render the graphics systems.
+
+*Note: These codes were written as a part of Computer Graphics project in 2nd year 1st semester of M.Tech (CS) in Indian Statistical Institute. Course was taken by Prof. Swagatam Das*
+
+###Files:
+
+--------------
+
+The project contains
+ - A C++ code file ( **cube.cpp** )
+ - A Make file to compile and link with Open GL ( **Makefile**)
+
+###Running the project:
+Make sure your open GL is installed & configured properly, then execute these in the directory the code is residing.
+```
+make
+./cube
+```
+
---
+++ cube.cpp
@@ -0,0 +1,326 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+#include <iostream>
+#include <cstdlib>
+//#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+using namespace std;
+//random number
+float random(int max)
+{
+    return rand() / (RAND_MAX / max + 1);
+}
+
+//Called when a key is pressed
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+		case 27: //Escape key
+			exit(0);
+	}
+}
+
+//Initializes 3D rendering
+void initRendering() {
+	glEnable(GL_DEPTH_TEST);
+}
+
+//Called when the window is resized
+void handleResize(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (double)w / (double)h, 1.0, 200.0);
+}
+float _x=5,_y=5,_z=5;
+void specialKeys( int key, int x, int y ) {
+ 
+  //  Right arrow - increase rotation by 5 degree
+  if (key == GLUT_KEY_RIGHT)
+  {  _y += 5; _x += 3;
+     _z += 2;
+  }  
+  //  Left arrow - decrease rotation by 5 degree
+  else if (key == GLUT_KEY_LEFT)
+  {  _y -= 5; _x -= 3;
+     _z -= 2;
+  }
+  else if (key == GLUT_KEY_UP)
+  {  _x += 5; _x +=3;
+     _z -= 2;
+  }
+  else if (key == GLUT_KEY_DOWN)
+  {  _x -= 5; _x +=3;
+     _z -= 2;
+  }
+  //  Request display update
+  glutPostRedisplay();
+ 
+}
+//void display();
+//void specialKeys();
+// Global Variables
+//double rotate_y=0; 
+//double rotate_x=0;
+
+float _angle = 30.0f;
+float _cameraAngle = 0.0f;
+
+
+//Draws the 3D scene
+void drawScene() {
+
+    
+    srand((unsigned int)time(0));
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+	glLoadIdentity(); //Reset the drawing perspective
+	glRotatef(-_cameraAngle, 0.0f, 1.0f, 0.0f); //Rotate the camera
+	glTranslatef(0.0f, 0.0f, -5.0f); //Move forward 5 units
+	
+	glPushMatrix(); //Save the transformations performed thus far
+	glTranslatef(0.0f, 0.0f, 0.0f); //Move to the center of the trapezoid
+	//_x=random(10);
+	//_y=random(10);
+	//_z=random(10);
+	glScalef(1.0f,1.0f,1.0f);//twice the size
+	glRotatef(-_x, 1.0f, 0.0f, 0.0f);//_x, _y,_z); //Rotate about the x-axis
+	glRotatef(_y+1, 0.0f, 1.0f, 0.0f);//about y
+	glRotatef(-_z, 0.0f, 0.0f, 1.0f);
+	
+	glBegin(GL_POLYGON);
+ 
+  glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  0.5, -0.5, -0.5 );      // P1 is red
+  glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  0.5,  0.5, -0.5 );      // P2 is green
+  glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -0.5,  0.5, -0.5 );      // P3 is blue
+  glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -0.5, -0.5, -0.5 );      // P4 is purple
+ 
+  glEnd();
+ 
+  // White side - BACK
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 1.0 );
+  glVertex3f(  0.5, -0.5, 0.5 );
+  glVertex3f(  0.5,  0.5, 0.5 );
+  glVertex3f( -0.5,  0.5, 0.5 );            // cube 1
+  glVertex3f( -0.5, -0.5, 0.5 );
+  glEnd();
+ 
+  // Purple side - RIGHT
+  glBegin(GL_POLYGON);
+  glColor3f(  1.0,  0.0,  1.0 );
+  glVertex3f( 0.5, -0.5, -0.5 );
+  glVertex3f( 0.5,  0.5, -0.5 );
+  glVertex3f( 0.5,  0.5,  0.5 );
+  glVertex3f( 0.5, -0.5,  0.5 );
+  glEnd();
+ 
+  // Green side - LEFT
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  1.0,  0.0 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+ 
+  // Blue side - TOP
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  0.0,  1.0 );
+  glVertex3f(  0.5,  0.5,  0.5 );
+  glVertex3f(  0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glEnd();
+ 
+  // Red side - BOTTOM
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  0.0,  0.0 );
+  glVertex3f(  0.5, -0.5, -0.5 );
+  glVertex3f(  0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+  glPopMatrix();
+  
+  glPushMatrix();
+  glTranslatef(1.0f,1.0f,1.0f);
+  
+  glScalef(0.4f,0.4f,0.4f);//40% the size
+  glRotatef(_x, 1.0f, 0.0f, 0.0f);//_x, _y,_z); //Rotate about the x-axis
+  glRotatef(-_y, 0.0f, 1.0f, 0.0f);//about y
+  glRotatef(_z, 0.0f, 0.0f, 1.0f);
+	
+  glBegin(GL_POLYGON);
+ 
+  glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  0.5, -0.5, -0.5 );      // P1 is red
+  glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  0.5,  0.5, -0.5 );      // P2 is green
+  glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -0.5,  0.5, -0.5 );      // P3 is blue
+  glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -0.5, -0.5, -0.5 );      // P4 is purple
+ 
+  glEnd();
+ 
+  // White side - BACK
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 1.0 );
+  glVertex3f(  0.5, -0.5, 0.5 );
+  glVertex3f(  0.5,  0.5, 0.5 );
+  glVertex3f( -0.5,  0.5, 0.5 );
+  glVertex3f( -0.5, -0.5, 0.5 );
+  glEnd();
+ 
+  // Purple side - RIGHT
+  glBegin(GL_POLYGON);
+  glColor3f(  1.0,  0.0,  1.0 );
+  glVertex3f( 0.5, -0.5, -0.5 );
+  glVertex3f( 0.5,  0.5, -0.5 );
+  glVertex3f( 0.5,  0.5,  0.5 );            // cube 2
+  glVertex3f( 0.5, -0.5,  0.5 );
+  glEnd();
+ 
+  // Green side - LEFT
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  1.0,  0.0 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+ 
+  // Blue side - TOP
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  0.0,  1.0 );
+  glVertex3f(  0.5,  0.5,  0.5 );
+  glVertex3f(  0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glEnd();
+ 
+  // Red side - BOTTOM
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  0.0,  0.0 );
+  glVertex3f(  0.5, -0.5, -0.5 );
+  glVertex3f(  0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+  glPopMatrix();
+  
+  
+  glPushMatrix();
+  glTranslatef(-3.0f,1.0f,-4.0f);
+  
+  glScalef(0.2f,0.2f,0.2f);//40% the size
+  glRotatef(_x-4, 1.0f, 0.0f, 0.0f);//_x, _y,_z); //Rotate about the x-axis
+  glRotatef(-_y-3, 0.0f, 1.0f, 0.0f);//about y
+  glRotatef(-_z-5, 0.0f, 0.0f, 1.0f);
+	
+  glBegin(GL_POLYGON);
+ 
+  glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  0.5, -0.5, -0.5 );      // P1 is red
+  glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  0.5,  0.5, -0.5 );      // P2 is green
+  glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -0.5,  0.5, -0.5 );      // P3 is blue
+  glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -0.5, -0.5, -0.5 );      // P4 is purple
+ 
+  glEnd();
+ 
+  // White side - BACK
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 1.0 );
+  glVertex3f(  0.5, -0.5, 0.5 );
+  glVertex3f(  0.5,  0.5, 0.5 );
+  glVertex3f( -0.5,  0.5, 0.5 );
+  glVertex3f( -0.5, -0.5, 0.5 );
+  glEnd();
+ 
+  // Purple side - RIGHT
+  glBegin(GL_POLYGON);
+  glColor3f(  1.0,  0.0,  1.0 );
+  glVertex3f( 0.5, -0.5, -0.5 );        //cube 3
+  glVertex3f( 0.5,  0.5, -0.5 );
+  glVertex3f( 0.5,  0.5,  0.5 );
+  glVertex3f( 0.5, -0.5,  0.5 );
+  glEnd();
+ 
+  // Green side - LEFT
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  1.0,  0.0 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+ 
+  // Blue side - TOP
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  0.0,  1.0 );
+  glVertex3f(  0.5,  0.5,  0.5 );
+  glVertex3f(  0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glEnd();
+ 
+  // Red side - BOTTOM
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  0.0,  0.0 );
+  glVertex3f(  0.5, -0.5, -0.5 );
+  glVertex3f(  0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+  glPopMatrix();
+  glFlush();
+  glutSwapBuffers();
+  
+  
+}
+void update(int value) {
+	//_angle += 2.0f;
+	//if (_angle > 360) {
+	//	_angle -= 360;
+	//}
+	
+	_x+= random(5);
+	_y+= random(5);
+	_z+= random(5);
+	if(_x>360)
+	_x-=360;
+	if(_y>360)
+	_y-=360;
+    if(_z>360)
+	_z-=360;
+
+	glutPostRedisplay(); //Tell GLUT that the display has changed
+	
+	//Tell GLUT to call update again in 25 milliseconds
+	glutTimerFunc(25, update, 0);
+}
+
+int main(int argc, char** argv) {
+	//Initialize GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 800);
+	
+	//Create the window
+	glutCreateWindow("3D cubes with random rotation about their x,y,z axes");
+	initRendering();
+	
+	//Set handler functions
+	glutDisplayFunc(drawScene);
+	//glutSpecialFunc(specialKeys);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	
+	glutTimerFunc(25, update, 0); //Add a timer
+	
+	glutMainLoop();
+	return 0;
+}
+
